0cc4f5993c16ea9208009a7c0d9ca74fc90b0839
Maqatur <maqaturs@gmail.com>
2024-03-17 17:19:49 +0400

    Exception 1

---
+++ src/test/java/ru/netology/homework/exceptions/ProductTest.java
@@ -0,0 +1,53 @@
+package ru.netology.homework.exceptions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+    Product product = new Product (5, "First", 50);
+
+    @Test
+    public void findByIdPositiveTest() {
+        ShopRepository shopRepository = new ShopRepository();
+        shopRepository.findById(5);
+
+        int expected = 5;
+        int actual = product.getId();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void findByIdNegativeTest() {
+        ShopRepository shopRepository = new ShopRepository();
+        shopRepository.findById(3);
+
+        Product expected = null;
+        Product actual = shopRepository.findById(3);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void removeTest() {
+        ShopRepository shopRepository = new ShopRepository();
+        shopRepository.remove(5);
+
+        Product[] expected = new Product[0];
+        Product[] actual = shopRepository.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void notFoundExceptionsTest() {
+        ShopRepository shopRepository = new ShopRepository();
+        shopRepository.remove(-1);
+
+        Assertions.assertThrows(NotFoundException.class, () ->{
+            shopRepository.remove(-1);
+        });
+    }
+
+}
